8af85d74a7498f794dc47754cbb11f13d02c2944
Roman Viktorovich <roman.viktorovich@outlook.com>
2022-02-10 12:03:56 +0300

    useState(Создаем состояние и учимся его изменять)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from "react";
+// Когда значение в input совпадает с состояние. Подобные компоненты называются управляемые
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [count, setCount] = useState(5)
+    const [value, setValue] = useState("Text in input")//добились синхронизации
+    console.log(count)
+    console.log(setCount)
 
+    const increment = () => {
+       setCount(count + 1)
+    }
+    const decrement = () => {
+        setCount(count - 1)
+    }
+    return (
+        <div className="App">
+            <h1>{count}</h1>
+            <h1>{value}</h1>
+            <input type="text" value={value}  onChange={event => setValue(event.target.value)}/> //добились синхронизации
+            <button onClick={increment}>increment</button>
+            <button onClick={decrement}>decrement</button>
+        </div>
+    );
+}
 export default App;
