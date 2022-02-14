61206659b0d1de984bd4f55521f76ab4583a14e1
Roman Viktorovich <roman.viktorovich@outlook.com>
2022-02-14 16:05:13 +0300

    create component(создание компонента)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, {useState} from "react";
-// Когда значение в input совпадает с состояние. Подобные компоненты называются управляемые
+import Counter from "./components/counter";
 function App() {
-    const [count, setCount] = useState(5)
-    const [value, setValue] = useState("Text in input")//добились синхронизации
-    console.log(count)
-    console.log(setCount)
 
-    const increment = () => {
-       setCount(count + 1)
-    }
-    const decrement = () => {
-        setCount(count - 1)
-    }
     return (
         <div className="App">
-            <h1>{count}</h1>
-            <h1>{value}</h1>
-            <input type="text" value={value}  onChange={event => setValue(event.target.value)}/> //добились синхронизации
-            <button onClick={increment}>increment</button>
-            <button onClick={decrement}>decrement</button>
+         <Counter/>
+         <Counter/>
+         <Counter/>
+         <Counter/>
         </div>
     );
 }
